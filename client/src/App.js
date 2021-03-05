39827657ba3bdbda377fe31e4ca7b040fc395122
Alex <alex96ford19@gmail.com>
2021-03-05 17:17:40 +0000

    Added dedux store and connected it to the project. Alerts are now shown when a users adds two password that don't match.

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,21 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import "./App.css";
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
+  <Provider store={store}>
   <Router>
   <Fragment>
     <NavBar/>
     <Route exact path="/" component={Landing}/>
     <section className="container">
+    <Alert/>
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
@@ -19,6 +26,7 @@ const App = () => (
     </section>
   </Fragment>
   </Router>
+  </Provider>
 );
 
 export default App;
