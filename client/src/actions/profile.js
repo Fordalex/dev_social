63fd322a92e72a0d3e450444ef3c1c6805ff5748
Alex <alex96ford19@gmail.com>
2021-03-09 10:07:11 +0000

    Added the component page to add a user, also the action to call the route and redirect the user.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,6 +6,8 @@ import {
     PROFILE_ERROR
 } from './types';
 
+const stringifyObject = require('stringify-object');
+
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
     try {
@@ -16,6 +18,45 @@ export const getCurrentProfile = () => async dispatch => {
             payload: res.data
         })
     } catch (err) {
+        dispatch({ 
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+         })
+    }
+}
+
+//  Create of update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+        if (!edit) {
+            history.push('/dashboard');
+        }
+
+    } catch (err) {
+
+        console.error(err)
+
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+
         dispatch({ 
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
