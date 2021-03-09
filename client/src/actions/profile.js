d0e166ca924cbbe47c923c56bcd01038952b4591
Alex <alex96ford19@gmail.com>
2021-03-09 11:06:53 +0000

    Added the form to edit the users profile.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -43,9 +43,9 @@ export const createProfile = (formData, history, edit = false) => async dispatch
 
         dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
 
-        if (!edit) {
-            history.push('/dashboard');
-        }
+        
+        history.push('/dashboard');
+        
 
     } catch (err) {
 
